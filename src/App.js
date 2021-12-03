a259bc74637e8a7fa89fd378438cdbb86e3a0128
Simon Zhang <simonzhang0422@gmail.com>
2021-12-03 16:30:28 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { ReactComponent as DNAIcon } from "./DNA.svg"
 import timelineElements from './timelineElements';
 import {VerticalTimeline, VerticalTimelineElement} from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-
+import githublogo from './github.png';
 
 function App() {
 
@@ -43,16 +43,15 @@ function App() {
         <div>
           <p id="description">In 1962, James Watson, Francis Crick, and Maurice Wilkins were awarded the Nobel Prize in Physiology or Medicine. However, Rosalind Franklin was not part of this group. In 1953, Rosalind Franklin was working under Maurice Wilkins, although somewhat independently. Wilkins showed Watson and Crick her findings and when Watson and Crick published their discoveries in 1953, they did not credit Franklin's photographs/research. Furthermore, at most three can be awarded a single Nobel Prize. Rosalind Franklin passed away 4 years before 1962 from ovarian cancer. The Nobel Prize committee generally did not award posthumously. </p>
         </div>
-        
+        <a href="https://docs.google.com/document/d/1C_viEjXykR7Io5zQ3n24Bo6-hv5k2ii21iT9w4GcYds/edit?usp=sharing"><h3 className="sources">Sources</h3></a>
+
         </div>
       </div>
      
-      <div className="info">
-        <a href="https://docs.google.com/document/d/1C_viEjXykR7Io5zQ3n24Bo6-hv5k2ii21iT9w4GcYds/edit?usp=sharing"><h3 className="sources">Sources</h3></a>
-      </div>
-
       <div className="footer">
-        <p>By Simon Zhang on Dec 2, 2021</p>
+        <a href="https://github.com/SimonZhang04/GeneticsTimeline">
+        <img className='github' src={githublogo} alt='github'/>
+        </a>
       </div>
       
     </div>
